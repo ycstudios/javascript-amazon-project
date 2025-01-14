c5a4ebeb592f7ea17b55a1ad526dc526f51ebe3a
Yashdev <yashchandanshive18@gmail.com>
2025-01-14 22:03:33 +0530

    submit

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,14 +1,25 @@
-export const deliveryOptions=[{
-    id:'1',
-    deliveryDay:7,
-    priceCents:0
-},{
-    id:'2',
-    deliveryDay:3,
-    priceCents:499
-},{
-    id:'3',
-    deliveryDay:1,
-    priceCents:999
-}
-]
+export const deliveryOptions = [{
+    id: '1',
+    deliveryDays: 7,
+    priceCents: 0
+  }, {
+    id: '2',
+    deliveryDays: 3,
+    priceCents: 499
+  }, {
+    id: '3',
+    deliveryDays: 1,
+    priceCents: 999
+  }];
+  
+  export function getDeliveryOption(deliveryOptionId) {
+    let deliveryOption;
+  
+    deliveryOptions.forEach((option) => {
+      if (option.id === deliveryOptionId) {
+        deliveryOption = option;
+      }
+    });
+  
+    return deliveryOption || deliveryOptions[0];
+  }
