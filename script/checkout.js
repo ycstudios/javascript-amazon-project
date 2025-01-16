05b1db4ba505b86790ca4a1917220eb6a4aa5a08
Yashdev <yashchandanshive18@gmail.com>
2025-01-16 09:32:29 +0530

    add backend parc file

--- script/checkout.js
+++ script/checkout.js
@@ -1,6 +1,7 @@
 import {renderOrderSummary} from './checkout/orderSumary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 // import '../data/cart-class.js';
+import '../data/backend-practice.js'
 
 renderOrderSummary();
 renderPaymentSummary();
