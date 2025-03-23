63d124b54377d46a6e6386b74a1aa664882dfae2
Yashdev <yashchandanshive18@gmail.com>
2025-03-23 11:57:36 +0530

    Your commit message here

--- script/checkout/orderSumary.js
+++ script/checkout/orderSumary.js
@@ -1,10 +1,10 @@
 import {cart, removeFromCart, updateDeliveryOption} from '../../data/cart.js';
 import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
-import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+// import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 import {renderPaymentSummary} from './paymentSummary.js';
-
+const today = dayjs();
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
 
